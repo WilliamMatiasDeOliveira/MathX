dbcb2826212ef8a7257df8435ba1f6427455b2a2
william <williammatiasjau@hotmail.com>
2024-10-21 08:17:00 -0300

    003_create_view_home

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,25 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
 
-    public function home()
+    public function home(): View
     {
-        echo 'pagina inicial';
+       return view('home');
     }
+
     public function generate_exercises(Request $request)
     {
         echo 'gerar exercicios';
     }
+
     public function print_exercises()
     {
         echo 'imprimir exercicios';
     }
+
     public function export_exercises()
     {
         echo 'exportar exercicios';
     }
+    
 }
